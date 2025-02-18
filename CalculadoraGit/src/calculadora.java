e73512282b33899791a2122d39beea89cb44c907
rafael.garcia <Rafael.garcia21@outlook.com>
2025-02-18 09:48:21 -0300

    adicionar metodo subtrair

--- CalculadoraGit/src/calculadora.java
+++ CalculadoraGit/src/calculadora.java
@@ -2,7 +2,11 @@
 public class calculadora {
     public static int somar(int a, int b) {
         return a + b;
+
     }
+    public static int subtrair (int a, int b) {
+            return a-b;
+        }
     public static void main(String[] args) {
         System.out.println(somar(5, 3));
 
